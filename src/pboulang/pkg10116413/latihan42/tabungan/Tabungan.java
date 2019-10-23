b5d562e886eaa496fdbb1b62e0cf0822909bc1d0
Harry Apriadi <harryapriadi1@gmail.com>
2019-10-23 15:33:45 +0700

    PBOUlang-10116413-Latihan42-Tabungan

---
+++ src/pboulang/pkg10116413/latihan42/tabungan/Tabungan.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboulang.pkg10116413.latihan42.tabungan;
+
+/**
+ *
+ * @author Lenovo
+ * NAMA         : HARRY APRIADI
+ * KELAS        : PBOULANG
+ * NIM          : 10116413
+ * DESKRIPSI    : 
+ */
+public class Tabungan {
+
+    private int saldo;
+
+    public int tabungan(int saldo) {
+
+        return this.saldo = saldo;
+    }
+
+    public int ambilUang(int jumlah) {
+
+        return jumlah;
+    }
+
+}
